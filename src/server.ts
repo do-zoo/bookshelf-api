ada5fb07ed2744be948cb7842aae82918c5adc6a
edwardedo603 <edwardedo603@gmail.com>
2023-09-30 18:51:17 +0700

    initial project

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import { Server } from '@hapi/hapi';
+
+const init = async () => {
+
+    const server: Server = new Server({
+        port: 9000,
+        host: 'localhost'
+    });
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
